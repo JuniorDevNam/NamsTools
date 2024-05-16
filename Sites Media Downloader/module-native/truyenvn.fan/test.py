f981066b2dfc6e6fcbbc1b826e4d6387fe7d64a0
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-16 23:20:32 +0700

    .

--- Sites Media Downloader/module-native/truyenvn.fan/test.py
+++ Sites Media Downloader/module-native/truyenvn.fan/test.py
@@ -1,3 +1,10 @@
 import requests
-web = "https://truyenvn.lol/truyen-tranh/dong-ho-ngung-dong-thoi-gian/chapter-26"
+web = "https://nettruyencc.com/truyen-tranh/blue-archive-global/chapter-1/1"
 print(web.split("/"))
+reffer_list=[
+   'https://nettruyenco.vn/',
+   'https://nettruyencc.com/'
+]
+print(f'https://{web.split("/")[2]}')
+print(reffer_list[1])
+print(reffer_list[1]==f'https://{web.split("/")[2]}')
