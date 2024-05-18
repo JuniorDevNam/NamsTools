ad4476d6087bbbe4eee0f1af129668438dab215f
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-19 00:51:21 +0700

    update

--- Sites Media Downloader/module-native/43nt@ivn/210vn.py
+++ Sites Media Downloader/module-native/43nt@ivn/210vn.py
@@ -3,8 +3,18 @@ from bs4 import BeautifulSoup
 from os.path import join
 from os import makedirs
 import sys
+import time
+import random
 
 #https://stackoverflow.com/questions/14587728/what-does-this-error-in-beautiful-soup-means
+user_agent_list = [
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
+   "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+   "Mozilla/5.0 (iPad; CPU OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/104.0.5112.99 Mobile/15E148 Safari/604.1",
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3",
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0"
+]
+
 
 def onechapter(web):
     output_dir = join(sys.path[0],"downloads")
@@ -31,19 +41,36 @@ def onechapter(web):
             f.write(res.content)
  
 
-def multichapters(link, dir, from_chapter, latest_chapter, *special_chapters):
-    listchaps = []
-    for c in range(from_chapter,latest_chapter+1):
-        chapterlink = f'{link}chapter-{c}'
-        listchaps.append(chapterlink)
-        if special_chapters != 0:
-            for x in special_chapters:
-                c = c + x
-                specialchapterlink = f'{link}chapter-{c}'
-                c = c - x
-                listchaps.append(specialchapterlink)
-    for x in listchaps:
-        onechapter(x, dir)
+def allchapters(web, headers):
+    res = requests.get(web,headers=headers)
+    html_content = res.text
+    soup = BeautifulSoup(html_content, 'html.parser')
+    #debug
+    #with open(debug_html, 'w') as f:
+    #    f.write(soup)
+    # Find all <a> tags within the specified <div>
+    links = []
+    for item in soup.find_all("div", class_="page-info"):
+        for link in item.find_all("a"):
+            links.append(link.get("href"))
+    print(links)
 
-onechapter("https://hentaivn.lat/36055-67235-xem-truyen-goblin-kara-hajimeru-sekai-seifuku-chap-1.html")
-#multichapters("https://khotruyen.cc/truyen-tranh/ba-chi-chu-nha-the-owner-of-a-building/chapter-115/","\downloads",110,115,0)
+
+web = str(input("Nhập đường link của truyện: "))
+print("**!** Tool còn nhiều hạn chế, và mình sẽ luôn cố gắng cập nhật để bắt kịp với trang web.")
+time.sleep(5)
+print("Running...")
+referer = f'https://{web.split("/")[2]}/'
+print("Server:",referer)
+headers = {
+   'Connection': 'keep-alive',
+   'Cache-Control': 'max-age=0',
+   'Upgrade-Insecure-Requests': '1',
+   'User-Agent': random.choice(user_agent_list),
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+   'Accept-Encoding': 'gzip, deflate',
+   'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+   #'referer': random.choice(reffer_list)
+   'referer': referer
+    }
+allchapters(web, headers)
