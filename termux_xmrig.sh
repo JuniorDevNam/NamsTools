ef4c0f41c9ebfc9653c1268ae53521e7725d38a5
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-24 20:54:16 +0700

    .

--- termux_xmrig.sh
+++ termux_xmrig.sh
@@ -6,5 +6,6 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build
 cd xmrig/build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
+sudo ./scripts/enable_1gb_pages.sh
 #./xmrig -o pool.hashvault.pro:443 -u 43KkmRcqPvN8fYXNxX5CeV1NWZ5wunUTSVocU7ArEjQG9aDoHsM8xf665Z9K5Dwsm34tdsJuTXboji9mgPzSRrmHG8cAf2x -p NamAndroidMining -k --coin monero --tls
-./xmrig -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 --coin=monero -u 3LHpkPBNRwXhJ7ttGYD5ToWmGgQ1PE7zGe.yhbul7aaguviak4 -k --nicehash
+./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 --coin=monero -u 3LHpkPBNRwXhJ7ttGYD5ToWmGgQ1PE7zGe.salad_termux_android -k --nicehash
--- termux_xmrig_unminer_doge.sh
+++ termux_xmrig_unminer_doge.sh
@@ -3,4 +3,5 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build
 cd xmrig/build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
-./xmrig -o stratum+ssl://rx.unmineable.com:443 -k -u DOGE:DSbF5Nsq3NJLZ59gCZ7GpaR3hGRgCTYSFP.unmineable_android_miner --no-color --http-port=60070 -a rx
+sudo ./scripts/enable_1gb_pages.sh
+./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+ssl://rx.unmineable.com:443 -k -u DOGE:DSbF5Nsq3NJLZ59gCZ7GpaR3hGRgCTYSFP.unmineable_android_miner --no-color --http-port=60070 -a rx
