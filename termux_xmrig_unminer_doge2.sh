0ebd2bdb0a8eb3199c2e83dde68910cd6ff78884
JuniorDevNam <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-25 18:43:06 +0700

    Update termux_xmrig_unminer_doge2.sh

--- termux_xmrig_unminer_doge2.sh
+++ termux_xmrig_unminer_doge2.sh
@@ -3,4 +3,4 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build
 cd xmrig/build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
-./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+ssl://rx.unmineable.com:443 -k -u DOGE:DSbF5Nsq3NJLZ59gCZ7GpaR3hGRgCTYSFP.unmineable_android_miner_2 --no-color --http-port=60070 -a rx
+./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+ssl://rx.unmineable.com:443 -k -u DOGE:DSbF5Nsq3NJLZ59gCZ7GpaR3hGRgCTYSFP.unmineable_android_miner_2 --http-port=60070 -a rx
