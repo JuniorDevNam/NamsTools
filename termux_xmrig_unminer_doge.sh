19d3f98bbafdc055a09f0b5e7247cdfc1880cd71
JuniorDevNam <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-25 14:15:17 +0700

    Update termux_xmrig_unminer_doge.sh

--- termux_xmrig_unminer_doge.sh
+++ termux_xmrig_unminer_doge.sh
@@ -1,7 +1,8 @@
-pkg install git build-essential cmake -y
+pkg install git tsu build-essential cmake -y
 git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build
 cd xmrig/build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
-sudo ./scripts/enable_1gb_pages.sh
+wget https://raw.githubusercontent.com/xmrig/xmrig/master/scripts/enable_1gb_pages.sh && chmod +x enable_1gb_pages.sh
+sudo ./enable_1gb_pages.sh
 ./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+ssl://rx.unmineable.com:443 -k -u DOGE:DSbF5Nsq3NJLZ59gCZ7GpaR3hGRgCTYSFP.unmineable_android_miner --no-color --http-port=60070 -a rx
