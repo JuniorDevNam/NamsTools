812d488f5c79616cde32f823ae21a68b355c408d
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-05-28 17:35:36 +0700

    .

--- termux_xmrig_2.sh
+++ termux_xmrig_2.sh
@@ -7,4 +7,4 @@ mkdir xmrig/build
 cd xmrig/build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
 #./xmrig -o pool.hashvault.pro:443 -u 43KkmRcqPvN8fYXNxX5CeV1NWZ5wunUTSVocU7ArEjQG9aDoHsM8xf665Z9K5Dwsm34tdsJuTXboji9mgPzSRrmHG8cAf2x -p NamAndroidMining -k --coin monero --tls
-./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 --coin=monero -u 3LHpkPBNRwXhJ7ttGYD5ToWmGgQ1PE7zGe.yhbul7aaguviak4 -k --nicehash
+./xmrig --cpu-priority=5 --cpu-no-yield --randomx-1gb-pages -o stratum+tcp://randomxmonero.auto.nicehash.com:9200 --coin=monero -u 3LHpkPBNRwXhJ7ttGYD5ToWmGgQ1PE7zGe.salad_termux_android -k --nicehash
