bc7629d82d477fc60676bdc2d1f622c8e876b24a
IAmNamNguyen <138494161+JuniorDevNam@users.noreply.github.com>
2024-08-24 00:12:35 +0700

    im tired

--- spectre-termux-miner.sh
+++ spectre-termux-miner.sh
@@ -3,7 +3,8 @@
 #apt upgrade
 pkg install git build-essential cmake clang openssl udns fmt libc++ lld -y
 git clone https://github.com/Tritonn204/tnn-miner.git
-mkdir tnn-miner/build
-cd tnn-miner/build
+cd tnn-miner
+mkdir build
+cd build
 cmake .. -DWITH_HWLOC=OFF && make -j$(nproc)
 ./tnn-miner --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qr7zs4m3xacnhhnwsktds5rujyqv94edmzeqjxtrrwwnpsxvv5jczj0x4cw0n --threads 8 --dev--fee 1
